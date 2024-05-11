c196b257079bba86dc49fc4e129a122735884922
Isaac-Saleh <118036130+Isaac-Saleh@users.noreply.github.com>
2024-05-11 14:40:50 +0800

    feat: Added the functions for fetching api data and returning. for both LoremIpsum and Password Generator

--- phase-4/assets/js/scriptPhase-4.js
+++ phase-4/assets/js/scriptPhase-4.js
@@ -21,16 +21,76 @@ function urlViaCheckboxSelection() {
     return url;
 }
 
+//Fetches the LoremIpsum paragraphs
+function fetchLoremIpsum(url) {
+    let paragraph =document.getElementById('lorem-ipsum').value;
+    if(parseInt(paragraph) < 1) {
+        alert("Number of Paragraphs cannot be less than 1!")
+        return;
+    }
+    fetch(url + paragraph, {
+        method: 'GET',
+        headers: {'X-Api-Key': API_KEY},
+        contentType: 'application/json'
+    })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+            throw response.status
+        })
+        .then(function (data) {
+            console.log(data.text)
+            const postContainer = document.getElementById('api_output')
+            postContainer.innerHTML = data.text;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            const errorContainer = document.getElementById('api_error_message');
+            errorContainer.innerHTML = (`Error: ${error} API call was Unsuccessful` )
+
+        });
+}
+
+function fetchPassword(url) {
+    let length = parseInt(document.getElementById('password_length').value)
+    console.log(length)
+    // if(length < 8) {
+    //     alert("We recommend Password Length to be greater than 8 characters")
+    //     return;
+    // }
+    fetch(url + length, {
+        method: 'GET',
+        headers: {'X-Api-Key': API_KEY},
+        contentType: 'application/json'
+    })
+        .then(function (response){
+            if(response.ok){
+                return response.json()
+            }
+            throw response.status;
+        })
+        .then(function (data) {
+            console.log(data)
+            const postContainer = document.getElementById('api_output');
+            postContainer.innerHTML = data.random_password;
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            const errorContainer = document.getElementById('api_error_message');
+            errorContainer.innerHTML = (`Error: ${error} API call was Unsuccessful` )
+        });
+}
 
 
 function onButtonClick (event) {
     event.preventDefault();
     url = urlViaCheckboxSelection();
     if(url === url1) {
-        fetchLoremIpsum(url); //to write
+        fetchLoremIpsum(url);
     }
     else if(url === url2) {
-        fetchPassword(url); // to write
+        fetchPassword(url);
     }
     else alert('You must select an API to call!')
 }
