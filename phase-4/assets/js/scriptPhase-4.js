f70cbb66e8e4674afbb8d09cff84de9724c54d9d
Isaac-Saleh <118036130+Isaac-Saleh@users.noreply.github.com>
2024-05-11 13:55:46 +0800

    feat: connecting the correct form to the chosen api and fetch function - TODO: Function to write

--- phase-4/assets/js/scriptPhase-4.js
+++ phase-4/assets/js/scriptPhase-4.js
@@ -4,6 +4,7 @@ const url1 = 'https://api.api-ninjas.com/v1/loremipsum?paragraphs='
 const url2 = 'https://api.api-ninjas.com/v1/passwordgenerator?length='
 let url = '';
 
+//Grabs the URL based on the Radio button Selection
 function urlViaCheckboxSelection() {
     if(document.getElementById('loremipsum').checked) {
         url = url1;
@@ -20,4 +21,21 @@ function urlViaCheckboxSelection() {
     return url;
 }
 
+
+
+function onButtonClick (event) {
+    event.preventDefault();
+    url = urlViaCheckboxSelection();
+    if(url === url1) {
+        fetchLoremIpsum(url); //to write
+    }
+    else if(url === url2) {
+        fetchPassword(url); // to write
+    }
+    else alert('You must select an API to call!')
+}
+
+
+document.getElementById('submit_button_lorem-ipsum').addEventListener("click", onButtonClick)
+document.getElementById('submit_button_password').addEventListener("click", onButtonClick)
 
